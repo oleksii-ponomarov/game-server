9c041aa779ba31288bae4204b34e6fc5704161e6
Oleksii Ponomarov <oleksii.ponomarov@okami.tech>
2022-02-02 12:52:18 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (_req, res) => {
+  res.json({
+    easyMode: {
+      field: 5,
+    },
+    normalMode: {
+      field: 10,
+    },
+    hardMode: {
+      field: 15,
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Game server is listening on ${port}`);
+});
